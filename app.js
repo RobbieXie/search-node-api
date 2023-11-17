058736560bd29c4d5e1dc2d990c02f9c2ccebaa7
RobbieXie <tiandi_xie@qq.com>
2023-11-17 22:56:00 +0800

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+
+const searchEngineTool = require("./searchEngineTool");
+
+const app = new Koa();
+app.use(bodyParser());
+
+app.use(async ctx => {
+  // the parsed body will store in ctx.request.body
+  // if nothing was parsed, body will be an empty object {}
+
+  let question = ctx.request.body.q;
+  ctx.body = { q: question, results: await searchEngineTool(question, 'bing') };
+});
+
+app.listen(3000);
